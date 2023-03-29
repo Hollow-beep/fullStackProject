c2a6f8288cf5c057e622b62f87c4d0830f660350
hollowK <hiep.lp206@gmail.com>
2023-03-29 23:59:42 +0700

    update new service

--- src/main/java/dev/hieplp/moviespring/MovieRepository.java
+++ src/main/java/dev/hieplp/moviespring/MovieRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.io.ObjectInput;
+import java.util.Optional;
 
+//Data access layer
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbID);
 }
--- src/main/java/dev/hieplp/moviespring/MovieService.java
+++ src/main/java/dev/hieplp/moviespring/MovieService.java
@@ -1,16 +1,28 @@
 package dev.hieplp.moviespring;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+//Use this Repository to get data from Database then return to API layer
 @Service
 public class MovieService {
+
     @Autowired
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> singleMovie(String imdbID){
+        return movieRepository.findMovieByImdbId(imdbID);
     }
 }
 
