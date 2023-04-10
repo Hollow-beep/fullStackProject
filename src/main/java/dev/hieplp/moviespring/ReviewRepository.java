1362308a68c082b4e581ccde4f9dba5ad537b625
hollowK <hiep.lp206@gmail.com>
2023-04-10 22:14:39 +0700

    add comment

--- src/main/java/dev/hieplp/moviespring/ReviewRepository.java
+++ src/main/java/dev/hieplp/moviespring/ReviewRepository.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+//Data access layer
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 
--- src/main/java/dev/hieplp/moviespring/ReviewService.java
+++ src/main/java/dev/hieplp/moviespring/ReviewService.java
@@ -16,9 +16,9 @@ public class ReviewService {
     public Review createReview(String reviewBody, String imdbID){
         //Create review object with new review body
         Review review = new Review(reviewBody);
-        //
+        //Insert review body to Data access Layer
         reviewRepository.insert(review);
-        //
+        //Update data to specific movie class in MongoDB database base on ImdbId
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbID))
                 .apply(new Update().push("reviewIds").value(review))
